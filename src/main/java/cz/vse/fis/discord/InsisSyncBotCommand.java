f2e7335e276486cd59820fa867b9f0abed445e2c
Jiří Vrba <jiri@vrba.dev>
2024-09-20 21:54:14 +0200

    Remove the default micronaut verbose flag + add log upon startup

--- src/main/java/cz/vse/fis/discord/InsisSyncBotCommand.java
+++ src/main/java/cz/vse/fis/discord/InsisSyncBotCommand.java
@@ -1,28 +1,20 @@
 package cz.vse.fis.discord;
 
 import io.micronaut.configuration.picocli.PicocliRunner;
-import io.micronaut.context.ApplicationContext;
-
-import picocli.CommandLine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
 
-@Command(name = "insis-sync-bot", description = "...",
-    mixinStandardHelpOptions = true)
+@Command(name = "insis-sync-bot", description = "...", mixinStandardHelpOptions = true)
 public class InsisSyncBotCommand implements Runnable {
 
-    @Option(names = {"-v", "--verbose"}, description = "...")
-    boolean verbose;
+    private final Logger logger = LoggerFactory.getLogger(InsisSyncBotCommand.class);
 
     public static void main(String[] args) throws Exception {
         PicocliRunner.run(InsisSyncBotCommand.class, args);
     }
 
     public void run() {
-        // business logic here
-        if (verbose) {
-            System.out.println("Hi!");
-        }
+        logger.info("Starting the InSIS sync bot.");
     }
 }
